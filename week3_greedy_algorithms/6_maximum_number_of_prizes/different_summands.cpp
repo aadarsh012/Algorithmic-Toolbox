06d4ebfd5c50b2944017dd6258513cfefe4d1193
aadarsh012 <aadarsh012@gmail.com>
2022-01-13 23:44:35 +0530

    Added week 3 problem 6

--- week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.cpp
+++ week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.cpp
@@ -3,18 +3,37 @@
 
 using std::vector;
 
-vector<int> optimal_summands(int n) {
+vector<int> optimal_summands(int n)
+{
   vector<int> summands;
   //write your code here
+  int v = 1, sum = 0;
+  while (sum < n)
+  {
+    sum += v;
+    if (n - sum > v)
+    {
+      summands.push_back(v);
+      v++;
+    }
+    else
+    {
+      sum -= v;
+      summands.push_back(n - sum);
+      sum = n;
+    }
+  }
   return summands;
 }
 
-int main() {
+int main()
+{
   int n;
   std::cin >> n;
   vector<int> summands = optimal_summands(n);
   std::cout << summands.size() << '\n';
-  for (size_t i = 0; i < summands.size(); ++i) {
+  for (size_t i = 0; i < summands.size(); ++i)
+  {
     std::cout << summands[i] << ' ';
   }
 }
