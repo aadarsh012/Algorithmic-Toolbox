1894fc66efa08473f102a810ee3ce0f70724f1a8
aadarsh012 <aadarsh012@gmail.com>
2022-01-26 16:45:41 +0530

    Added Problems

--- week4_divide_and_conquer/3_majority_element/majority_element.cpp
+++ week4_divide_and_conquer/3_majority_element/majority_element.cpp
@@ -1,68 +1,80 @@
-// #include <iostream>
-// #include <vector>
+#include <iostream>
+#include <vector>
 
-// //freopen("input.txt","r",stdin);
-// //freopen("output.txt","w",stdout);
+using namespace std;
 
-// using namespace std;
+void merge(vector<int> &a, int start, int mid, int end)
+{
+    int i = start, j = mid + 1;
+    vector<int> res;
+    while (i <= mid && j <= end)
+    {
+        if (a[i] < a[j])
+        {
+            res.push_back(a[i++]);
+        }
+        else
+        {
+            res.push_back(a[j++]);
+        }
+    }
 
-// int ContainMajority(vector<int> Arr, int l, int u)
-// {
+    while (i <= mid)
+    {
+        res.push_back(a[i++]);
+    }
+    while (j <= end)
+    {
+        res.push_back(a[j++]);
+    }
+    for (int k = start; k <= end; k++)
+    {
+        a[k] = res[k - start];
+    }
+}
 
-//   if (u == 0)
-//   {
-//     return Arr[0];
-//   }
+void mergeSort(vector<int> &a, int start, int end)
+{
+    if (start >= end)
+    {
+        return;
+    }
+    else
+    {
+        int mid = (start + end) / 2;
+        mergeSort(a, start, mid);
+        mergeSort(a, mid + 1, end);
+        merge(a, start, mid, end);
+    }
+}
 
-//   int majorL = ContainMajority(Arr, l, u / 2);
-//   int majorU = ContainMajority(Arr, (u / 2) + 1, u);
-
-//   int count = 0;
-//   for (int i = 0; i <= u; i++)
-//   {
-//     if (Arr[i] == majorL)
-//     {
-//       count++;
-//     }
-//   }
-//   if (count > (u + 1) / 2)
-//   {
-//     return majorL;
-//   }
-
-//   count = 0;
-//   for (int i = 0; i <= u; i++)
-//   {
-//     if (Arr[i] == majorU)
-//     {
-//       count++;
-//     }
-//   }
-//   if (count > (u + 1) / 2)
-//   {
-//     return majorU;
-//   }
-
-//   return -1;
-// }
-// int main()
-// {
-//   int n;
-//   cin >> n;
-
-//   vector<int> Arr(n);
-//   for (int i = 0; i < n; i++)
-//     cin >> Arr[i];
-
-//   int res = ContainMajority(Arr, 0, n - 1);
-//   if (res == -1)
-//   {
-//     cout << 0 << endl;
-//   }
-//   else
-//   {
-//     cout << res << endl;
-//   }
-
-//   return 0;
-// }
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    mergeSort(a, 0, n - 1);
+    int counter = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (a[i] == a[i + 1])
+        {
+            counter++;
+            if (counter + 1 > n / 2)
+            {
+                cout << 1 << endl;
+                return 0;
+            }
+        }
+        else
+        {
+            counter = 0;
+        }
+    }
+    cout << 0 << endl;
+    return 0;
+}
