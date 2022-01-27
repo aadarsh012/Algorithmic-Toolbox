8dcb1096065f7120dbd3290371fca287bcaa18c5
aadarsh012 <aadarsh012@gmail.com>
2022-01-27 16:59:45 +0530

    Problem update

--- week4_divide_and_conquer/5_number_of_inversions/inversions.cpp
+++ week4_divide_and_conquer/5_number_of_inversions/inversions.cpp
@@ -4,82 +4,38 @@
 
 using namespace std;
 
-// void deletion(vector<int> &a, int index)
-// {
-//   for (int i = index; i < a.size(); i++)
-//   {
-//     a[i] = a[i + 1];
-//   }
-// }
-
-pair<vector<int>, int> Merge(vector<int> &a, vector<int> &b)
+int merge(vector<int> &a, int start, int mid, int end)
 {
+  int i = start, j = mid + 1, counter = 0;
 
-  int n = a.size();
-  int m = b.size();
-
-  cout << m << " " << n << endl;
-
-  int count = 0, i = 0, j = 0;
-  vector<int> res(n + m);
-  while (i < n && j < m)
+  while (i <= mid)
   {
-
-    if (a[i] <= b[j])
+    j = mid + 1;
+    while (j <= end)
     {
-      res.push_back(a[i]);
-      i++;
+      if (a[i] > a[j++])
+      {
+        counter++;
+      }
     }
-    else
-    {
-      count++;
-      res.push_back(b[j]);
-      j++;
-    }
+    i++;
   }
 
-  while (i < n)
-  {
-    res.push_back(a[i++]);
-  }
-
-  while (j < m)
-  {
-    res.push_back(b[j++]);
-  }
-
-  return make_pair(res, count);
+  return counter;
 }
 
-pair<vector<int>, int> MergeSort(vector<int> &a, int l, int r)
+int mergeSort(vector<int> &a, int start, int end)
 {
   int count = 0;
-  pair<vector<int>, int> A, B, Result;
-  if (r == l)
+  if (start < end)
   {
-    return make_pair(a, count);
+    int mid = (start + end) / 2;
+    count += mergeSort(a, start, mid);
+    count += mergeSort(a, mid + 1, end);
+    count += merge(a, start, mid, end);
   }
-
-  int mid = (r + l) / 2;
-  A = MergeSort(a, l, mid);
-  B = MergeSort(a, mid + 1, r);
-  Result = Merge(A.first, B.first);
-  count += Result.second;
-
-  return make_pair(Result.first, count);
+  return count;
 }
-
-// long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right)
-// {
-//   long long number_of_inversions = 0;
-//   if (right <= left + 1)
-//     return number_of_inversions;
-//   size_t ave = left + (right - left) / 2;
-//   number_of_inversions += get_number_of_inversions(a, b, left, ave);
-//   number_of_inversions += get_number_of_inversions(a, b, ave, right);
-//   //write your code here
-//   return number_of_inversions;
-// }
 
 int main()
 {
@@ -91,11 +47,7 @@ int main()
     cin >> a[i];
   }
 
-  pair<vector<int>, int> result = MergeSort(a, 0, n - 1);
+  int result = mergeSort(a, 0, n - 1);
 
-  for (int i = 0; i < n; i++)
-  {
-    cout << result.first[i] << " ";
-  }
-  // cout << result.second << '\n';
+  cout << result << endl;
 }
